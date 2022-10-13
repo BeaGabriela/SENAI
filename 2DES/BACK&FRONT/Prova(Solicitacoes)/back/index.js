86aa0746b4a86266873c46515ea4578409652519
BeaGabriela <biiagabrielaa@gmail.com>
2022-10-13 11:31:46 -0300

    e

--- 2DES/BACK&FRONT/Prova(Solicitacoes)/back/index.js
+++ 2DES/BACK&FRONT/Prova(Solicitacoes)/back/index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
+const PORT = process.env.PORT || 3000
 const express = require('express');
 const cors = require('cors');
 
 const itens = require('./src/routes/solicitacao');
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(itens);
-
-app.listen(5000, () => {
-    console.log("Respondendo na porta 5000");
+app.use(express.json())
+.use(cors())
+.use(itens)
+.listen(PORT, () => {
+    console.log("Respondendo na porta 5000" + PORT);
 });
