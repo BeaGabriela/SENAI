daf25baf22f0532f3a06bc31639370b349064fdb
BeaGabriela <biiagabrielaa@gmail.com>
2022-11-08 11:32:16 -0300

    front

---
+++ 2DES/FRONTEND/Trabalho_frontEnd/index.js
@@ -0,0 +1,7 @@
+function abrirModal(){
+    var modal = document.querySelector('.modal');
+    modal.classList.remove('model')
+ var cadastrar =  document.querySelector('.cadastrar')
+   cadastrar.classList.remove('model')
+  
+}
