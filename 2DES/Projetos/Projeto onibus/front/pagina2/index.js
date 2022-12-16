156695ca6bda1c8894878285c351b9fa587d9b3b
BeaGabriela <biiagabrielaa@gmail.com>
2022-12-16 09:14:27 -0300

    e

--- 2DES/Projetos/Projeto onibus/front/pagina2/index.js
+++ 2DES/Projetos/Projeto onibus/front/pagina2/index.js
@@ -1,6 +1,3 @@
-
-
-
 function carregar () {
     fetch('http://localhost:3000/onibus/ladoDireito')
     .then(resp => {return resp.json()})
@@ -8,7 +5,7 @@ function carregar () {
         data.forEach(e => {
             var oni = document.querySelector('.onibus').cloneNode(true);
             oni.classList.remove('model')
-            oni.querySelector('#assento').innerHTML =` <button onclick="buscarA(${e.assentoD})">A${e.assentoD} </button>`
+            oni.querySelector('#assento').innerHTML =` <button id='btnCick' onclick="buscarA(${e.assentoD})">A${e.assentoD} </button>`
 
             document.querySelector('.aside1').appendChild(oni)
             
@@ -21,7 +18,7 @@ function carregar () {
         data.forEach(e => {
             var oni = document.querySelector('.onibus1').cloneNode(true);
             oni.classList.remove('model')
-            oni.querySelector('#assento1').innerHTML = `<button onclick="buscarB(${e.assentoD})">B${e.assentoD} </button>`
+            oni.querySelector('#assento1').innerHTML = `<button id='btnCick' onclick="buscarB(${e.assentoD})">B${e.assentoD} </button>`
 
             document.querySelector('.aside2').appendChild(oni)
         })
@@ -33,7 +30,7 @@ function carregar () {
         data.forEach(e => {
             var oni = document.querySelector('.onibus2').cloneNode(true);
             oni.classList.remove('model')
-            oni.querySelector('#assento2').innerHTML = `<button onclick="buscarC(${e.assentoD})">C${e.assentoD} </button>`
+            oni.querySelector('#assento2').innerHTML = `<button id='btnCick' onclick="buscarC(${e.assentoD})">C${e.assentoD} </button>`
 
             document.querySelector('.aside3').appendChild(oni)
         })
@@ -45,7 +42,7 @@ function carregar () {
         data.forEach(e => {
             var oni = document.querySelector('.onibus3').cloneNode(true);
             oni.classList.remove('model')
-            oni.querySelector('#assento3').innerHTML = `<button id='btn' onclick="buscarD(${e.assentoD})">D${e.assentoD} </button>`
+            oni.querySelector('#assento3').innerHTML = `<button id='btnCick' onclick="buscarD(${e.assentoD})">D${e.assentoD} </button>`
 
             document.querySelector('.aside4').appendChild(oni)
         })
