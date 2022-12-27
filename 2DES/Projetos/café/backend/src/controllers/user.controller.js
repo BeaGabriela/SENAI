2a2ca18e368a72463b9d7a61fcbcd5b254e001e6
BeaGabriela <biiagabrielaa@gmail.com>
2022-12-27 15:55:00 -0300

    cafe

--- 2DES/Projetos/café/backend/src/controllers/user.controller.js
+++ 2DES/Projetos/café/backend/src/controllers/user.controller.js
@@ -2,11 +2,11 @@ const ligacao = require('../models/users.model.js')
 const con = require('../dao/cafeteria.dao.js')
 
 const listandoUsers = (req,res) =>{
-    con.query(ligacao.lendoUsuarios(req.body), (err, result) => {
-        if(err = null){
+    con.query(ligacao.lendoUsuarios(), (err, result) => {
+        if(err == null){
             res.json(result).end()
         }else{
-            res.status(500).end()
+            res.json(err).end()
         }
     })
 }
