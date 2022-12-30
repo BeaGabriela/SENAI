14959c0922131f5efb3171dcca119bc66b7ff233
BeaGabriela <biiagabrielaa@gmail.com>
2022-12-30 13:59:37 -0300

    react e cafe

--- 2DES/JAVASCRIPT/REACT/Aula1/my-app/src/App.js
+++ 2DES/JAVASCRIPT/REACT/Aula1/my-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <h1>Testando Eventos</h1>
       <Evento numero='1' />
-      <Evento numero='2' />
+      {/* <Evento numero='2' /> */}
       <Form />
       {/* <h1>Testando o CSS</h1>
       <Frase/>
--- 2DES/JAVASCRIPT/REACT/Aula1/my-app/src/components/Eventos.js
+++ 2DES/JAVASCRIPT/REACT/Aula1/my-app/src/components/Eventos.js
@@ -1,13 +1,20 @@
+import Button from './eventos/Button.js'
 function Evento({numero}) {
 
     function meuEvento(){
-        console.log(`Opa, fui ativado  ${numero}`)
+        console.log(`Ativando 1 evento`)
+    }
+
+    function segundo(){
+        console.log('Ativando o seundo evento')
     }
 
     return(
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar</button>
+            <Button event={meuEvento} text='Primeiro Evento'/>
+            <Button event={segundo} text='Segundo Evento'/>
+            {/* <button onClick={meuEvento}>Ativar</button> */}
         </div>
     )
 }
