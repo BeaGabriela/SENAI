02f0de34bcfaa177562e8d7f5da7006b80150065
BeaGabriela <biiagabrielaa@gmail.com>
2023-01-10 14:31:37 -0300

    cafe

--- 2DES/Projetos/café/frontend/LOGIN/login.js
+++ 2DES/Projetos/café/frontend/LOGIN/login.js
@@ -15,8 +15,8 @@ function validarLogin() {
                     validar
                     info.innerHTML = ''
                     info.innerHTML = 'Senha ou email incorretos'
-                    
-                } else if( inputEmail.value == login.email && inputsenha.value == login.senha) {
+
+                } else if (inputEmail.value == login.email && inputsenha.value == login.senha) {
                     info.innerHTML = 'Bem-vindo'
                     validar = true
                     inputEmail.style.border = '2px solid green'
@@ -30,6 +30,59 @@ function validarLogin() {
 }
 
 
-function carregar(){
+function carregar() {
+    var pags = document.querySelector('.Paginas')
+    var pag1 = document.querySelector('.pagina1')
+    var pag2 = document.querySelector('.pagina2')
+    var pag3 = document.querySelector('.pagina3')
+    var pag4 = document.querySelector('.pagina4')
+    var btnPag1 = document.querySelector('#irPraPag2')
+    var btnPag2 = document.querySelector('#irPraPag3')
+    var btnPag3 = document.querySelector('#irPraPag4')
+    var finalizar = document.querySelector('#finalizar')
     var criarNovoCadastro = document.querySelector('#criarNovoCadastro')
+
+    var InputUsuario = document.querySelector('#usuario')
+    var InputEmailNovo = document.querySelector('#inputEmailNovo')
+    var InputSenhaNovo = document.querySelector('#inputSenhaNovo')
+    var InputConfirmarSenha = document.querySelector('#confirmarSenha')
+
+    var NomeUsario
+    var email
+    var senha
+    var ConfirmarSenha
+
+        criarNovoCadastro.addEventListener('click', () => {
+            pags.classList.remove('modal')
+            pag1.classList.remove('modal')
+
+            btnPag1.addEventListener('click', () => {
+                pag1.classList.add('modal')
+                pag2.classList.remove('modal')
+            })
+            btnPag2.addEventListener('click', () => {
+                pag1.classList.add('modal')
+                pag2.classList.add('modal')
+                pag3.classList.remove('modal')
+            })
+            btnPag3.addEventListener('click', () => {
+                pag1.classList.add('modal')
+                pag2.classList.add('modal')
+                pag3.classList.add('modal')
+                pag4.classList.remove('modal')
+            })
+            finalizar.addEventListener('click', () => {
+                NomeUsario = InputUsuario.value
+                email = InputEmailNovo.value
+                senha = InputSenhaNovo.value
+                ConfirmarSenha = InputConfirmarSenha.value
+                console.log(NomeUsario)
+                console.log(email)
+                console.log(senha)
+                console.log(ConfirmarSenha)
+                // window.location.reload()
+
+            })
+
+        })
 }
