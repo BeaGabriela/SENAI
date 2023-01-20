fe4bb97ee30a4f9f0c8172a76b441edf8959181a
BeaGabriela <biiagabrielaa@gmail.com>
2023-01-20 14:27:06 -0300

    cafe

--- 2DES/JAVASCRIPT/REACT/costs/src/App.js
+++ 2DES/JAVASCRIPT/REACT/costs/src/App.js
@@ -1,30 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/navbar'
+import Footer from './components/layout/footer'
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/newProject'>Novo Projeto</Link>
-        
-      </div>
+      <Navbar/>
       <Routes>
-        <Container customClass='min-height'>
+         <Container customClass='min-height'/>
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/company' element= {<Company/>} />
-        <Route exact path='/contact' element= {<Contact/>} />
+        <Route exact path='/contact' element= {<Contact/>} /> 
         <Route exact path='/newProject' element= {<NewProject/>} />
-        </Container>
+        
       </Routes>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
