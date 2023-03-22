9085f7ce6101f36aed9da62de14f67923871703d
BeaGabriela <biiagabrielaa@gmail.com>
2023-03-22 11:17:10 -0300

    youtube music

---
+++ 2DES/FRONTEND/Treinando/portifolo/Trabalho - Music/index.js
@@ -0,0 +1,10 @@
+function ImagemHover() {
+    var config = document.querySelector('.hover')
+    config.classList.remove('model')
+}
+
+
+function ImagemHoverSumiu(){
+    var config = document.querySelector('.hover')
+    config.classList.add('model')
+}
