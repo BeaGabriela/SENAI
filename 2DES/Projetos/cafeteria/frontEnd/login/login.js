53f3e704f49b64fa71700c69f8006a937e0fdf73
BeaGabriela <biiagabrielaa@gmail.com>
2023-03-31 08:28:50 -0300

    cafe

--- 2DES/Projetos/cafeteria/frontEnd/login/login.js
+++ 2DES/Projetos/cafeteria/frontEnd/login/login.js
@@ -1,5 +1,29 @@
-document.querySelector(".oi").classList.add("descendo")
+var altura = 0
+var largura = 0
+var alturaheader = document.querySelector('.oi')
+// alturaheader.classList.add("descendo")
+altura = window.innerWidth;
+largura = window.innerHeight;
 
-function entrar() {
+// console.log(altura, largura)
+
+const alturaHeader = document.querySelector(".oi");
+
+console.log(alturaHeader.getBoundingClientRect());
+
+const propriedades = alturaHeader.getBoundingClientRect();
+
+// // altura
+const altura1 = propriedades.y;
+if (alturaheader == 969) {
+    altura1.classList.add('.pause')
+}
+
+
+
+
+
+
+function Entrar() {
 
 }
