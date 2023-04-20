3a3af7e64e76bc22d9a5b2f77e56e0db690a1f1a
BeaGabriela <biiagabrielaa@gmail.com>
2023-04-20 08:59:48 -0300

    Alteraçãos no backend

--- 3DES/PROJETOS/Aula16(Trabalho)/Fisico/src/controllers/paciente.controller.js
+++ 3DES/PROJETOS/Aula16(Trabalho)/Fisico/src/controllers/paciente.controller.js
@@ -1,9 +1,16 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+// const pessoa = require('../models/pagamento')
 
 const iniciar = async (req, res) => {
     res.render('index')
 }
+
+// const modelarLista = (lista) => {
+//     for (i = 0; i < lista.length; i++)
+//         lista[i] = new pessoa(lista[i], "ler")
+//     return lista
+// }
 
 const criar = async (req, res) => {
     let dataAtual = new Date()
@@ -22,20 +29,16 @@ const criar = async (req, res) => {
         data: parse
     })
 
-    // let a = fisico[0]["idade"] = 5
+
+    Object.assign(parse, {
+        idade: Number(mes - req.body.nascimento.slice(0, 4)),
+        imc: Number(req.body.peso / (req.body.altura * req.body.altura)),
+        diagnostico: ''
+      });
 
 
 
-    res.json({
-        nome_completo: req.body.nome_completo,
-        nascimento: new Date(req.body.nascimento),
-        peso: Number(req.body.peso),
-        altura: Number(req.body.altura),
-        "idade": Number(mes - req.body.nascimento.slice(0, 4)),
-        "imc": Number(req.body.peso / (req.body.altura * req.body.altura)),
-        "diagnostico": 'OK?'
-    }
-    )
+    res.json(parse)
 }
 
 
@@ -43,7 +46,7 @@ const listar = async (req, res) => {
     let paciente = await prisma.Paciente.findMany()
     console.log(paciente)
     // res.json(paciente).status(200)
-    res.render('index', { pacientes: paciente })
+    res.render('index', { pacientes:paciente })
 }
 
 
