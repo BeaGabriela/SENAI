d0002692a9320885019bc74e0f2699756a32b373
BeaGabriela <biiagabrielaa@gmail.com>
2023-05-03 10:02:02 -0300

    Implantação do token para diferenciamento das gerenciamentos

---
+++ 3DES/PROJETOS/TechMan/backend/src/middleware/middleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const validaAcesso = (req, res, next) => {
+    let token = req.headers.authorization;
+
+    if(token) {
+        token = token.split(' ')[1];
+        jwt.verify(token, process.env.KEY, (err, data) => {
+            if(err != null) res.status(404).json(err).end();
+            else {
+                console.log(data)
+                if(data.Usuario.perfil === 1) {
+                    console.log(data)
+                    next();
+                }else {
+                    res.status(401).end();
+                }
+            }
+        })
+    }else {
+        res.status(401).end();
+    }
+}
+
+module.exports = {
+    validaAcesso
+}
