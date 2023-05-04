e6827264405c711a9a2f4e84bb360a28d332f537
BeaGabriela <biiagabrielaa@gmail.com>
2023-05-04 10:41:40 -0300

    Edição do banco de dados

--- 3DES/PROJETOS/TechMan/backend/prisma/migrations/20230504131927_init/migration.sql
+++ 3DES/PROJETOS/TechMan/backend/prisma/migrations/20230504131927_init/migration.sql
@@ -12,6 +12,7 @@ CREATE TABLE `Usuario` (
     `senha` VARCHAR(191) NOT NULL,
     `perfil` INTEGER NOT NULL,
 
+    UNIQUE INDEX `Usuario_senha_key`(`senha`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -19,7 +20,7 @@ CREATE TABLE `Usuario` (
 CREATE TABLE `Comentarios` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `comentario` VARCHAR(191) NOT NULL,
-    `equipamento` VARCHAR(191) NOT NULL,
+    `equipamento` INTEGER NOT NULL,
     `perfil` INTEGER NOT NULL,
     `data` DATETIME(3) NOT NULL,
 
@@ -43,3 +44,33 @@ ALTER TABLE `Usuario` ADD CONSTRAINT `Usuario_perfil_fkey` FOREIGN KEY (`perfil`
 
 -- AddForeignKey
 ALTER TABLE `Comentarios` ADD CONSTRAINT `Comentarios_perfil_fkey` FOREIGN KEY (`perfil`) REFERENCES `Perfil`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento.DESKTOP-CJ165MI/Desktop/SENAI/3DES/PROJETOS/TechMan/backend/dados/perfis.csv'
+INTO TABLE Perfil
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento.DESKTOP-CJ165MI/Desktop/SENAI/3DES/PROJETOS/TechMan/backend/dados/usuarios.csv'
+INTO TABLE Usuario
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento.DESKTOP-CJ165MI/Desktop/SENAI/3DES/PROJETOS/TechMan/backend/dados/equipamentos.csv'
+INTO TABLE Equipamentos
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento.DESKTOP-CJ165MI/Desktop/SENAI/3DES/PROJETOS/TechMan/backend/dados/comentarios.csv'
+INTO TABLE Comentarios
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
