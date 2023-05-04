85da2e43338ac77099dfb9074d6017710515c187
BeaGabriela <biiagabrielaa@gmail.com>
2023-05-04 10:42:07 -0300

    edição da rota de pesquisa

--- 3DES/PROJETOS/TechMan/backend/src/routes/comentarios.routes.js
+++ 3DES/PROJETOS/TechMan/backend/src/routes/comentarios.routes.js
@@ -7,7 +7,7 @@ const comentarios = require('../controllers/comentarios');
 
 router.post('/comentariosCreate', comentarios.create);
 router.get('/comentarios', comentarios.read);
-router.get('/comentarios/:id', comentarios.readOne);
+router.get('/comentarios/:equipamento', comentarios.readOne);
 router.put('/comentarios/:id', comentarios.update);
 router.delete('/comentarios/:id', comentarios.remove);
 
