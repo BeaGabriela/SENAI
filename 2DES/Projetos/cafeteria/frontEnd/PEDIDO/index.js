1ac92e05c6315376173308ce2e0fae726ff62e05
BeaGabriela <biiagabrielaa@gmail.com>
2023-06-26 14:07:51 -0300

    Editado o funcionamento da tela de escolha de proporção da comida/bebida

--- 2DES/Projetos/cafeteria/frontEnd/PEDIDO/index.js
+++ 2DES/Projetos/cafeteria/frontEnd/PEDIDO/index.js
@@ -1,8 +1,4 @@
 function carregar() {
-    // var tamanho = document.querySelector('.tamanhos')
-    // var tempe = document.querySelector('.temperaturas')
-    // var acucar = document.querySelector('.acucars')
-    // var leite = document.querySelector('.leites')
     var id_comida = localStorage.getItem('informacoes')
 
     var imagemPrincipal = document.querySelector('#imagemP')
@@ -15,43 +11,147 @@ function carregar() {
         .then(res => { return res.json() })
         .then(u => {
             u.forEach(comida => {
-                 console.log(comida)
+                console.log(comida)
                 if (comida.id == id_comida) {
                     imagemPrincipal.src = '../imgs/' + comida.img
-               
+
                     nomeComida.innerHTML = comida.nome
                     descricao.innerHTML = comida.descricao
+                    valorDefinido = comida.valor
                     valor.value = comida.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
                 }
-
-
             })
         })
+
+
+    //Setando valores para o tamanho
+    var tamanhoDefinitivo = ''
+    var valorDefinido = 0
+    var valorDefinitivo = 0
+
+    //Definindo o filtro dos tamanhos
+    var selectTamanhos = document.querySelector('.tamanhos')
+    selectTamanhos.addEventListener('change', () => {
+        if (selectTamanhos.value == 'grande') {
+            tamanhoDefinitivo = 'grande'
+            valorDefinitivo = valorDefinido + 8
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        } else if (selectTamanhos.value == 'medio') {
+            tamanhoDefinitivo = 'medio'
+            valorDefinitivo = valorDefinido + 4
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        } else if (selectTamanhos.value == 'pequeno') {
+            tamanhoDefinitivo = 'pequeno'
+            valorDefinitivo = valorDefinido
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        } else {
+            console.log('vazio');
+            valorDefinitivo = valorDefinido
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        }
+    })
+
+
+
+
+    //Definindo valores para o acucar
+    var valorAcucar = 0
+    var tipoAcucar = ''
+    //Filtrando o select
+    var selectAcucar = document.querySelector('.acucars')
+    selectAcucar.addEventListener('change', () => {
+        if (selectAcucar.value == 'acucar') {
+            tipoAcucar = 'acucar'
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        } else if (selectAcucar.value == 'mel') {
+            tipoAcucar = 'mel'
+            valorAcucar = valorDefinitivo + 2
+            valor.value = valorAcucar.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        } else if (selectAcucar.value == 'adocante') {
+            tipoAcucar = 'adocante'
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        } else {
+            tipoAcucar = 'sem'
+            valor.value = valorDefinitivo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        }
+    })
+
+        //Setando o valor do leite
+        var valorLeite = 0
+        var tipoLeite = ''
+    
+        //Filtrando o select do leite
+        var selectLeite = document.querySelector('.leites')
+        selectLeite.addEventListener('change', () => {
+            if (selectLeite.value == 'comum') {
+                tipoLeite = 'comum'
+                valorLeite = valorDefinitivo + valorAcucar
+             console.log(valorLeite)
+                valor.value = valorLeite.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            } else if (selectLeite.value == 'amendoa') {
+                tipoLeite = 'amendoa'
+                valorLeite = valorDefinitivo + valorAcucar + 6
+                valor.value = valorLeite.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            } else if (selectLeite.value == 'coco') {
+                tipoLeite = 'coco'
+                valorLeite = valorDefinitivo + valorAcucar + 4
+                valor.value = valorLeite.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            } else {
+                tipoLeite = 'sem'
+                valor.value = valorAcucar.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            }
+        })
+
+        
+
+    //Setando temperatura
+    
+    var tipoTempertura = ''
+    //Filtrando o select
+    var selectTemperatura = document.querySelector('.temperaturas')
+    selectAcucar.addEventListener('change', () => {
+        if (selectTemperatura.value == 'quente') {
+            tipoTempertura = 'quente'
+            
+        } else if (selectTemperatura.value == 'morno') {
+            tipoTempertura = 'morno'
+        } else if (selectTemperatura.value == 'frio') {
+            tipoTempertura = 'gelado'
+        } else {
+            conolse.log('vazio')
+            
+        }
+    })
+
+
     var btnCompras = document.querySelector('#btnCompra')
     btnCompras.addEventListener('click', () => {
 
         var json = {
             "id_comida": id_comida,
-            "temperatura": tempe.value,
-            "tamanho": tamanho.value,
-            "leite": leite.value,
-            "adocar": acucar.value
+            "temperatura": tipoTempertura,
+            "tamanho": tamanhoDefinitivo,
+            "leite": tipoLeite,
+            "adocar": tipoAcucar
         }
 
-
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(json)
-        };
-
-        fetch('http://localhost:3000/cadastrarProporcoes', options)
-            .then(response => response.status)
-            .then(response => {
-                if (response == 200) {
-                    window.location.href = '../FINALIZAR/finalizar.html'
-                    localStorage.setItem('fim', JSON.stringify(json))
-                }
-            })
+        console.log(json)
+    
     })
 }
+//         const options = {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify(json)
+//         };
+
+//         fetch('http://localhost:3000/cadastrarProporcoes', options)
+//             .then(response => response.status)
+//             .then(response => {
+//                 if (response == 200) {
+//                     window.location.href = '../FINALIZAR/finalizar.html'
+//                     localStorage.setItem('fim', JSON.stringify(json))
+//                 }
+//             })
+//     })
+// }
