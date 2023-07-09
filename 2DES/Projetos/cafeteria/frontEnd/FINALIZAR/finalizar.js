5589f43e9cfef3a5f849774f475b88a191bf8282
BeaGabriela <biiagabrielaa@gmail.com>
2023-07-09 15:12:47 -0300

    criando o funcionamento da tela de finalizar

--- 2DES/Projetos/cafeteria/frontEnd/FINALIZAR/finalizar.js
+++ 2DES/Projetos/cafeteria/frontEnd/FINALIZAR/finalizar.js
@@ -3,4 +3,31 @@ function carregar(){
     resumo = JSON.parse(resumo)
 
     console.log(resumo)
+    validandoInputRetirada()
+}
+
+
+function validandoInputRetirada(){
+    var entrega = document.querySelector('#entregar')
+    var local = document.querySelector('#local')
+    var retirada = document.querySelector('#retirada')
+
+    entrega.addEventListener('change', () => {
+        console.log('entrega')
+    })
+
+    local.addEventListener('input', () => {
+        console.log('local')
+    })
+
+    retirada.addEventListener('input', () => {
+        console.log('retirada')
+    })
+
+
+
+
+
+
+    
 }
