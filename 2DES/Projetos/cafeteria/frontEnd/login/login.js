57f03c2c4b3e6f1cffe922e280270a296319ed0f
BeaGabriela <biiagabrielaa@gmail.com>
2023-07-23 14:34:35 -0300

    Arrumando a atela de login para adentrar ao app

--- 2DES/Projetos/cafeteria/frontEnd/login/login.js
+++ 2DES/Projetos/cafeteria/frontEnd/login/login.js
@@ -25,5 +25,32 @@ if (alturaheader == 969) {
 
 
 function Entrar() {
-    window.location.href= '../HOME/index.html'
+    // window.location.href= '../HOME/index.html'
+    var email = document.querySelector('#email')
+    var senha = document.querySelector('#password')
+
+    var json = {
+       "email": email.value,
+       "senha": senha.value
+    }
+
+    console.log(json) 
+
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(json)
+    };
+    
+    fetch('http://localhost:3000/usuario/login', options)
+    .then(response => response.status)
+    .then(response => {
+        if (response == 201) {
+            window.location.href = '../HOME/index.html'
+            // localStorage.setItem('fim', JSON.stringify(json))
+        }else{
+            console.log('erro')
+        }
+    })
+
 }
