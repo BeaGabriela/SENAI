c028a0f861561996a825d8c90b2f9cf4132d946f
BeaGabriela <biiagabrielaa@gmail.com>
2023-07-23 14:35:20 -0300

    Listando os pedidos e seu valor total

--- 2DES/Projetos/cafeteria/frontEnd/FINALIZAR/finalizar.js
+++ 2DES/Projetos/cafeteria/frontEnd/FINALIZAR/finalizar.js
@@ -1,11 +1,13 @@
-function carregar(){
-    var resumo = localStorage.getItem('fim')
-    resumo = JSON.parse(resumo)
+var id_user = localStorage.getItem('id_user')
+var resumo = localStorage.getItem('fim')
+resumo = JSON.parse(resumo)
 
-    console.log(resumo)
+
+function carregar(){
     // console.log(validandoInputRetirada())
     validandoInputRetirada()
     ValidandoPagamento()
+    RevisandoPedido()
 }
 
 function validandoInputRetirada() {
@@ -52,5 +54,43 @@ function validandoInputRetirada() {
          console.log('nada')
         }
     })
+  }
+
+  function RevisandoPedido(){
+    var valorTotalPedido = document.querySelector('#valorFinal')
+    const informacoes = JSON.parse(localStorage.getItem('fim')) || [];
+    console.log(informacoes.id_comida)
+     
+    var valorTotalTodosPedidos = 0;
+    // var totalValor = document.querySelector('#totalValor')
+
+    fetch(`http://localhost:3000/pedidos/${id_user}`)
+        .then(res => res.json())
+        .then(u => {
+            u.forEach(comida => {
+                var divPedidos = document.querySelector('.pedidoAnalise').cloneNode(true)
+                divPedidos.classList.remove('model')
+                    console.log(comida.nome)
+                    divPedidos.querySelector('#pedidoIMG').src = '../imgs/' + comida.comidas.img
+                    divPedidos.querySelector('#pedidoNome').innerHTML = comida.comidas.nome
+                    divPedidos.querySelector('#opcoesPedidoTempe').innerHTML = comida.temperatura
+                    divPedidos.querySelector('#opcoesPedidoAdocante').innerHTML = comida.acucar
+                    divPedidos.querySelector('#opcoesPedidoleite').innerHTML = comida.leite
+                    divPedidos.querySelector('#opcoesPedidoTamanho').innerHTML = comida.tamanho
+
+
+                    divPedidos.querySelector('#valorCadaPedido').innerHTML = comida.valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+                   
+                    
+                    valorTotalTodosPedidos += comida.valorTotal;
+                    console.log(valorTotalTodosPedidos)
+
+                    valorTotalPedido.innerHTML = valorTotalTodosPedidos.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+                    document.querySelector('.apendarPedido').appendChild(divPedidos)
+                // }
+
+            })
+        })
+
   }
   
