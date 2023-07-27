383163306d912608f91039b44b78b2b3f9c1ab56
BeaGabriela <biiagabrielaa@gmail.com>
2023-07-27 13:37:39 -0300

    Edição e aprimoriamento da função 'esqueci senha' e 'login'

--- 2DES/Projetos/cafeteria/frontEnd/login/login.js
+++ 2DES/Projetos/cafeteria/frontEnd/login/login.js
@@ -29,6 +29,9 @@ function Entrar() {
     var email = document.querySelector('#email')
     var senha = document.querySelector('#password')
 
+
+    var info = document.querySelector('#info')
+
     var json = {
        "email": email.value,
        "senha": senha.value
@@ -43,14 +46,41 @@ function Entrar() {
     };
     
     fetch('http://localhost:3000/usuario/login', options)
-    .then(response => response.status)
-    .then(response => {
-        if (response == 201) {
-            window.location.href = '../HOME/index.html'
-            // localStorage.setItem('fim', JSON.stringify(json))
+    .then((response) => {
+        if(response.status != 200){
+            info.innerHTML = 'ERRO no usuário. Tente novamente'
         }else{
-            console.log('erro')
+            info.innerHTML = ''
+            return response.json()
+        }
+    })
+    .then((resp) => {
+        if(email.value == 0 && senha.value == 0){
+           info.innerHTML = 'Preencha os campos!'
+        }else if(email.value != undefined && senha.value != undefined){
+            if(resp.nivel == 1){
+                info.innerHTML = ''
+                localStorage.setItem('user', JSON.stringify(resp))
+                window.location.href= '../HOME/index.html'
+            }else{
+                conosle.log('geremte')
+            }
         }
     })
 
+}
+
+function esqueceuSenha(){
+    var modalSenha = document.querySelector('.esqueceSenha')
+    modalSenha.classList.remove('model')
+}
+
+function VoltarTelaLogin(){
+    var modalSenha = document.querySelector('.esqueceSenha')
+    modalSenha.classList.add('model')
+}
+
+function cadastrarNovoUsuario(){
+    var cadastrarNovo = document.querySelector('.cadastrarNovo')
+    cadastrarNovo.classList.remove('model') 
 }
